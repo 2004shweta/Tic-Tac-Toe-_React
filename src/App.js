73d3f83adc413eafa785f039b56e316b03b3ccb8
2004shweta <156895477+2004shweta@users.noreply.github.com>
2024-07-12 01:06:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Tictactoe from './components/Tic-tac-toe/tic-tac-toe';
+
+function App() {
+  return (
+    <div >
+      <Tictactoe/>
+    
+    </div>
+  );
+}
+
+export default App;
